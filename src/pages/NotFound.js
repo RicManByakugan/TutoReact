7c2b5611be5e88eee97b2deb504d16d44873f85e
Ratovonirina <91662841+RicManByakugan@users.noreply.github.com>
2022-05-04 07:54:50 +0300

    Update NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -3,8 +3,7 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
     return ( 
         <div className="pageNotFound">
-            <h3 align="center">Page 404</h3>
-            <Link to="/">Revenir sur la page d'accueil</Link>
+            <Link to="/">React Pays</Link>
         </div>
     );
 }
